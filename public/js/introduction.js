5e4ed8c3d77a06525727c2048d6375cc1ac9d821
Tiffany Allen <t5allen@ucsd.edu>
2017-05-24 13:25:05 -0700

    working on introduction page

--- public/js/introduction.js
+++ public/js/introduction.js
@@ -2,6 +2,7 @@ var app = firebase.initializeApp(config);
 document.getElementById("logout").onclick = function(){
     console.log("Clicked logout");
     firebase.auth().signOut();
+    window.location.href = '/login.html';
 };
 
 document.getElementById("toMap").onclick = function(){
