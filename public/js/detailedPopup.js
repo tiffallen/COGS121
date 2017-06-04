cc85597dc3f1d11df8a1d02e9b457c0774871f54
Stephanie Villalobos <sgvillal@ucsd.edu>
2017-06-03 20:13:04 -0700

    detailedPopup.js commented

--- public/js/detailedPopup.js
+++ public/js/detailedPopup.js
@@ -10,17 +10,20 @@
   //var ref = firebase.database();
   var ref = firebase.database().ref("places");
 
+  // gets popup name and pop UID from local storage 
   var popupName = localStorage.getItem('popupName');
   var popupID = localStorage.getItem('popupUID');
 
+  // go back to previous page function
   var goBack = function goBack()
   {
     window.history.back();
   };
 
+  // set title to location name
   document.getElementById("popup-title").innerHTML = popupName;
 
-
+  //From firebase, search for popups UID to be able to access that place's data
   var ref = firebase.database().ref('places/' + popupID);
   console.log(popupName + "(" + popupID + ") : " + ref);
   ref.once('value').then(function(snapshot) { 
@@ -28,6 +31,7 @@
     //console.log(Object.keys(snapshot.val()));
     console.log("Snapshot.val = " + snapshot.val() + "Snapshot.key = " + snapshot.key);
 
+    // fetches all of locations data
     var coordinates = popUpArray.coordinates;
     var labels = popUpArray.labels;
     var category_icon = popUpArray.icon_img;
@@ -53,6 +57,8 @@
     var simLocUID2 =  popUpArray.simLocUID2;
     var simLocUID3 =  popUpArray.simLocUID3;
 
+
+    // loads page with that locations data 
     document.getElementById('popup-pics1').src = picture;
     document.getElementById('popup-pics2').src = picture2;
     document.getElementById('popup-pics3').src = picture3;
@@ -80,6 +86,7 @@
     started = true;
 
 
+    // loads that similar location suggestion' detailedPopup, when it is clicked
     $('#simLoc1').on('click', function() { popupRedirectFunction(simLoc1, simLocUID1); });
     $('#simLocPic1').on('click', function() { popupRedirectFunction(simLoc1, simLocUID1); });
     $('#simLoc2').on('click', function() { popupRedirectFunction(simLoc2, simLocUID2); });
@@ -97,7 +104,7 @@
     }
   });
 
-
+  // when checkin button is clicked, checkin count is incrememnted (once per page load)
   function checkinFunction() {
     if(buttonClicked == false) {
       ref.once('value').then(function(snapshot) { 
@@ -110,6 +117,11 @@
     }
   };
 
+  /* when you click the "Read More" button, it expands more information
+   * about that location, and changes the button to "Read Less"
+   * OR
+   * when you click "Read Less", it hides all but first paragraph of the article
+   */
   function readmoreFunction() {
     if(started == true) { 
       document.getElementById('article2').style.display = "inline-block";
@@ -128,7 +140,7 @@
     }
   };
 
-
+  // logs you out of UCSD Explorer
   $(function(){
   //logout
   document.getElementById("logout").onclick = function(){
@@ -138,7 +150,7 @@
 
 });
 
-
+/* Lets you go through the slides of all the pictures */
   var slideIndex = 1;
   showDivs(slideIndex);
 
@@ -158,6 +170,9 @@
   }
 
 
+
+// Finding most similar location
+
 var buildQuery = function buildQuery(term=null, matchWholePhrase=false, label=null)
 {
     // skeleton of the JSON object we will write to DB
@@ -170,6 +185,70 @@ var buildQuery = function buildQuery(term=null, matchWholePhrase=false, label=nu
 
   buildQueryBody(query, term, matchWholePhrase, label);
   return query;
+};
+
+var buildQueryBody = function buildQueryBody(query, term, matchWholePhrase, label)
+{
+    var body = query.body =
+    {    
+    };
+
+    body.query =
+    {
+        "bool":
+        {
+            "must":
+            [
+            ]
+        }
+    };
+
+    if(term)
+    {
+        if(matchWholePhrase)
+        {
+            body.query.bool.must.push(
+            {
+                "match_phrase":
+                {
+                    "_all": term
+                }
+            });
+        }
+        else
+        {
+            body.query.bool.must.push(
+            {
+                "match":
+                {
+                    "_all": term
+                }
+            });
+        }
+    }
+
+    if(!(label == null || label === "All" || label == false))
+    {
+        body.query.bool.must.push(
+        {
+            "match":
+            {
+                "labels": label
+            }
+        });
+    }
+};
+
+  // conduct a search by writing it to the search/request path
+  var doSearch = function doSearch(query, recenter=false)
+  {
+    console.log(query);
+    var ref = database.ref().child(PATH);
+    var key = ref.child('request').push(query).key;
+    ref.child('response/'+key).on('value', function(snap)
+    {
+        showResults(snap, recenter);
+    });
 };
 
   
