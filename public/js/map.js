cd9c213eb5ec60d75bbee83640243e723800619b
Stephanie Villalobos <sgvillal@ucsd.edu>
2017-06-29 01:00:17 -0700

    position start

--- public/js/map.js
+++ public/js/map.js
@@ -1200,9 +1200,9 @@ geolocation.on('change:position', function()
     positionFeature.setGeometry(position ? new ol.geom.Point(position) : null);
     if (!started)
     {
-        position = ol.proj.transform([-117.233949, 32.879943], 'EPSG:4326', 'EPSG:3857');
-        view.setCenter(position); //this gets annoying, only do it once on load
-        console.log("POSITION FOR LOCATION CENTER" + position);
+        // opening page position
+        positionStart = ol.proj.transform([-117.233949, 32.879943], 'EPSG:4326', 'EPSG:3857');
+        view.setCenter(positionStart); //this gets annoying, only do it once on load
         started = true;
     }
 });
